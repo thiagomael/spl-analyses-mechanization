8890aced0f013d04e95201422485bf3d3fae4a2d
vanderalves <vander.alves@gmail.com>
2019-01-14 15:58:42 -0200

    Update SPLTheory.hs
    For the sake of generality, back to Sven's original suggestion with names updated to paper's terminology.  For instance, the old assumption of hierarchical structure for the model is too limited, not even working for reliability

--- SPLTheory.hs
+++ SPLTheory.hs
@@ -7,28 +7,15 @@ import Prelude hiding (pi)
 
 ------ Non-variable definitions
 
--- A non-variable model is either a basic non-dividable construct or a nested structure
--- later on, the nesting could be considered as a list, or a normal form approach could be taken.
--- The initial spec and verification in PVS might be simpler with the current version below.
-data Product = Basic |                
-               Composite Product Product
+-- A non-variable model is an uninterpreted type abstracting the details of any particular product type
+data Product;
 
-data Property = SomeValue;
+data Property = SomeValue deriving Eq
 
--- Function operating on intermediate results during the analysis of non-variable models.
--- The idea is that this function provides a high-level view of the analysis function,
--- i.e., modulo the structural recursion.
-type  AnalyzeModelCompositeShell = Property -> Property -> Property
-
-analyzeModelCompositeShell :: AnalyzeModelCompositeShell 
-analyzeModelCompositeShell _ _ = SomeValue
-
--- To analyze a non-variable model, we return some uninterpreted value in the basic case 
--- or apply an analysis shell function to the intemediate results emerging from the recursive application
--- in the nested structure of the model 
+-- To analyze a non-variable model, we return some uninterpreted value since the analysis is given
+-- In other words, since the non-variable  model is uninterpreted, so is its analysis.
 alpha :: Product -> Property
-alpha Basic = SomeValue
-alpha (Composite m1 m2) =  analyzeModelCompositeShell (alpha m1) (alpha m2)
+alpha _  = SomeValue
 
 ------ variable definitions
 
@@ -36,39 +23,46 @@ alpha (Composite m1 m2) =  analyzeModelCompositeShell (alpha m1) (alpha m2)
 data PresenceCondition;
 
 -- A configuration values a presence condition 
-type Conf = PresenceCondition -> Bool
+type Conf = PresenceCondition -> Bool  
 
--- A variable model is either a base non-variable model, a variation point with choices depending on an option,
--- or a nested variant structure
+-- A variable model is either a non-variable model or a variation point with choices depending on an option,
 data AnnotativeModel = ModelBase Product | 
-                       ModelChoice PresenceCondition AnnotativeModel AnnotativeModel |
-                       ModelComposite  AnnotativeModel  AnnotativeModel
+                       ModelChoice PresenceCondition AnnotativeModel AnnotativeModel 
+                       
 
 -- The following function binds variability in a variable model for a given configuration via structural recursion.
 -- In case the model has no variability, the base model is returned. Otherwise, in case of a variation point,
--- the derivation continues within the choice corresponding to the configuration. In case of a composite variable model, 
--- a composite non-variable model is returned after binding the internal structure.
+-- the derivation continues within the choice corresponding to the configuration. 
 pi :: AnnotativeModel -> Conf -> Product
 pi (ModelBase m) _ = m
 pi (ModelChoice pc vm1 vm2) c = if c pc 
                                    then pi vm1 c 
                                    else pi vm2 c
-pi (ModelComposite vm1 vm2) c = Composite (pi vm1 c)  (pi vm2 c)
-				
--- Representation of variability bookeeping of analysis. This supports lazy computation of the analysis				
+
+                
+-- Representation of variability bookeeping of analysis. This supports lazy computation of the analysis             
 data AnnotativeExpression = BaseExpression Property | 
-                            ChoiceExpression PresenceCondition AnnotativeExpression AnnotativeExpression | 
-                            CompositeExpression AnalyzeModelCompositeShell AnnotativeExpression  AnnotativeExpression ;  
+                            ChoiceExpression PresenceCondition AnnotativeExpression AnnotativeExpression 
+                            
 
 -- Binds the variability in the variability bookeeping of analysis
 sigma :: AnnotativeExpression -> Conf -> Property
 sigma (BaseExpression p) _ = p
 sigma (ChoiceExpression pc vp1 vp2) c = if (c pc) then (sigma vp1 c) else (sigma vp2 c)
-sigma (CompositeExpression analysisShell vp1 vp2) c =  analysisShell (sigma vp1 c) (sigma vp2 c)
 
 -- Analyzes a variable model via structural recursion. In case the model has no variability, the non-variable 
 -- analysis is performed. Otherwise, variability bookeeping is performed considering the structure of variability.
 hatAlpha :: AnnotativeModel -> AnnotativeExpression
 hatAlpha (ModelBase m) = BaseExpression (alpha m)
 hatAlpha (ModelChoice pc vm1 vm2) = ChoiceExpression pc (hatAlpha vm1) (hatAlpha vm2)
-hatAlpha (ModelComposite vm1 vm2) = CompositeExpression analyzeModelCompositeShell(hatAlpha vm1) (hatAlpha vm2)
+
+
+-- specifying top-right quadrant commutativity
+--conf: VAR Conf
+--vModel : VAR AnnotativeModel
+--commutative_product_family_product: THEOREM sigma(hatAlpha(vModel),conf) = alpha(pi(vModel,conf))
+
+commutative_product_family_product :: Conf -> AnnotativeModel -> Bool
+
+commutative_product_family_product conf  vModel = sigma (hatAlpha vModel) conf ==  alpha(pi vModel conf)
+
