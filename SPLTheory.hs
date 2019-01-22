cd002d943c739535d4f6ed144f6b09d46805c36d
vanderalves <vander.alves@gmail.com>
2019-01-22 12:20:32 -0200

    Update SPLTheory.hs
    Refinement of the top-left quadrant, considering concrete implementation for partial composition of models and expressions, which are leveraged by pi' and sigma'. Again, some isomorphism seems to exist between compositional model and compositional expressions. Since sigma' and pi' have similar behavior, this seems to guarantee the commutativity. A conjecture is that the proof should have be same structure than of the top-right quadrant.

--- SPLTheory.hs
+++ SPLTheory.hs
@@ -71,19 +71,40 @@ commutative_product_family_product conf  vModel = sigma (hatAlpha vModel) conf =
 -- from well-founded relation. Type class??
 -- all finite, including list
 top :: (Functor t) =>  t AnnotativeModel  -> AnnotativeModel
-dependents :: (Functor t) => t AnnotativeModel ->  AnnotativeModel -> [AnnotativeModel]
+dependents :: (Functor t) => t AnnotativeModel ->  AnnotativeModel -> [(PresenceCondition, t AnnotativeModel)]
 
--- needed for the composition in each node of the relation
-partialCompostion ::  AnnotativeModel -> Product -> AnnotativeModel
-   
+-- needed for the model composition in each node of the relation
+-- incorporating dependent models from the recurseve composition (pi')  along the structure
+pMdlComp ::  AnnotativeModel -> Product -> AnnotativeModel
+pMdlComp (ModelChoice pc (ModelChoice pc1 am11 am12 ) vp2@(ModelChoice pc2 am21 am22 ) ) p = 
+         (ModelChoice pc (ModelBase p) vp2 )
+pMdlComp (ModelChoice pc  prdt@(ModelBase productct) (ModelChoice pc2 am21 am22 ) ) p = 
+         (ModelChoice pc  prdt (ModelBase p) )       
+pMdlComp am _ = am 
+ 
 pi' ::  (Functor t) => Conf -> t AnnotativeModel ->  Product
-pi' c cm = foldl partialCompostion (top cm) (map (pi' c) (dependents cm (top cm)))
+pi' c cm = foldl pMdlComp 
+                (top cm) 
+                (map (\(pc,scm) -> if (c pc) then  pi' c scm else ModelBase Product) (dependents cm (top cm)))
   
 analyzeCM :: (Functor t) => t AnnotativeModel -> t AnnotativeExpression
 analyzeCM cm = fmap hatAlpha cm 
 
-sigma' ::  (Functor t) => Conf -> t AnnotativeExpression -> Property  
-sigma'  c  ce =  substitute (top ce) (map (sigma' c) (dependents ce (top ce)))
+-- needed for the partial expression composition in each node of the relation
+-- incorporating dependent values from the recursieve evaluation (sigma') along the structure
+pExpComp ::  AnnotativeExpression -> Property -> AnnotativeExpression
+pExpComp (ChoiceExpression pc (ChoiceExpression pc1 vp11 vp12) vp2@(ChoiceExpression pc2 vp21 vp22) ) p   =  
+         (ChoiceExpression pc (BaseExpression p) vp2)
+pExpComp (ChoiceExpression pc vp1@(BaseExpression pr) (ChoiceExpression pc2 vp21 vp22) ) p   =  
+         (ChoiceExpression pc vp1 (BaseExpression p))        
+pExpComp  ae  _  =  ae 
+
+sigma' :: (Functor t) => Conf -> t AnnotativeExpression -> Property  
+sigma' c  ce = sigma (foldl pExpComp 
+                            (top ce) 
+                            (map (\(pc, sce)-> if (c pc) then sigma' c sce else SomeValue) 
+                                 (dependents ce (top ce)))) 
+                     c
 
 commutative_feature_product_product :: (Functor t) => Conf -> t AnnotativeModel -> Bool
 
